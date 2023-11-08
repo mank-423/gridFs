d7a4af994e08761c4b865da79096477b38a9a973
Mayank Kumar <96490105+mank-423@users.noreply.github.com>
2023-11-08 18:19:05 +0530

    Working api of upload download delete

---
+++ api/database/config.js
@@ -0,0 +1,14 @@
+//config.js
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_USER_PWD}@mernproject.bicx37m.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`);
+		console.log("MongoDB connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ api/utils/upload.js
@@ -0,0 +1,24 @@
+//upload.js
+const multer = require("multer");
+const { GridFsStorage } = require("multer-gridfs-storage");
+
+// Create storage engine
+function upload() {
+  const mongodbUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_USER_PWD}@mernproject.bicx37m.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+  const storage = new GridFsStorage({
+    url: mongodbUrl,
+    file: (req, file) => {
+      return new Promise((resolve, _reject) => {
+        const fileInfo = {
+          filename: file.originalname,
+          bucketName: "filesBucket",
+        };
+        resolve(fileInfo);
+      });
+    },
+  });
+
+  return multer({ storage });
+}
+
+module.exports = { upload };
